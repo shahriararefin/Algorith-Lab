de80997e8d7d0af979ae69ae06fc7bd1d5c266b1
shahriararefin <shahriararefin7@gmail.com>
2023-01-31 18:17:22 +0600

    psuh

---
+++ InsertionSort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+    int array[10];
+    int j, key, size;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &size);
+
+    printf("Enter the array elements: ");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+
+    for (int i = 1; i < size; i++)
+    {
+        key = array[i];
+        j = i - 1;
+
+        while (key < array[j] && j >= 0)
+        {
+            array[j + 1] = array[j];
+            --j;
+        }
+        array[j + 1] = key;
+    }
+
+    printf("Sorted array in ascending order:\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
