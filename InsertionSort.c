e664dff2c5ae69b65165c03960d09624d8acd89a
shahriararefin <shahriararefin7@gmail.com>
2023-02-08 08:40:02 +0600

    selection sort

--- InsertionSort.c
+++ InsertionSort.c
@@ -2,21 +2,23 @@
 int main()
 {
     int array[10];
-    int key, size;
+    int key, n;
 
     printf("Enter the number of elements in the array: ");
-    scanf("%d", &size);
+    scanf("%d", &n);
 
     printf("Enter the array elements: ");
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < n; i++)
     {
         scanf("%d", &array[i]);
     }
 
-    for (int i = 1; i < size; i++)
+    for (int i = 1; i < n; i++)
     {
         key = array[i];
         int j = i - 1;
+     //   printf("%d %d \n", key,j);
+
 
         while (key < array[j] && j >= 0)
         {
@@ -24,10 +26,11 @@ int main()
             --j;
         }
         array[j + 1] = key;
+       // printf("%d \n", array[j + 1]);
     }
 
     printf("Sorted array in ascending order:\n");
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < n; i++)
     {
         printf("%d ", array[i]);
     }
--- SelectionSort.c
+++ SelectionSort.c
@@ -1,6 +1,43 @@
 #include<stdio.h>
 int main(){
 
+    int n,i;
+    scanf("%d",&n);
+
+    int arr[21];
+    for ( i = 0; i < n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    
+
+    int min=i;
+
+    for ( i = 0; i < n-1; i++)
+    {
+        min= i;
+
+        for (int j = i+1; j < n; j++)
+        {
+            if (arr[j]<arr[min])
+                min = j;
+
+            if (min!=i)
+            {
+                int temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+            
+            
+        }
+        
+    }
+    
+    for ( i = 0; i < n; i++)
+    {
+        printf("%d ",arr[i]);
+    }
     
 
     return 0;
